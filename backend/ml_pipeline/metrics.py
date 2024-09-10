9f2cd389f359051d7ac0b09cdef0b954c976388f
Evgenii Iurin <iurin.evgenii@gmail.com>
2024-09-10 09:33:27 +0200

    Add trainig script

---
+++ backend/ml_pipeline/metrics.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def smape(preds, target):
+    n = len(preds)
+    masked_arr = ~((preds == 0) & (target == 0))
+    preds, target = preds[masked_arr], target[masked_arr]
+    num = np.abs(preds - target)
+    denom = np.abs(preds) + np.abs(target)
+    smape_val = (200 * np.sum(num / denom)) / n
+    return smape_val
+
+def lgbm_smape(preds, train_data):
+    labels = train_data.get_label()
+    smape_val = smape(preds, labels)
+    return 'SMAPE', smape_val, False
--- backend/ml_pipeline/scripts/train.py
+++ backend/ml_pipeline/scripts/train.py
@@ -1,18 +1,15 @@
+import hydra
+from omegaconf import DictConfig
 import lightgbm as lgb
 import pandas as pd
 
-# # # remove during inference
-# import os
-# os.chdir('/mnt/c/Users/iurin/work/itmo-hackathon/coming_soon_ai_product_hack')
 
 from backend.ml_pipeline.preprocess_for_inference import calculate_features_for_inference 
 from backend.ml_pipeline.configs.feature_params import one_day_params
+from backend.ml_pipeline.model import LGBMModel
 
-def train(data: pd.DataFrame):
-    data_with_one_day_features = calculate_features_for_inference(data=data, params=one_day_params)
-    data_with_one_day_features = data_with_one_day_features.drop(columns=['date'])
-    data = data_with_one_day_features.copy()
 
+def run_training(data: pd.DataFrame, config: DictConfig):
     # shift target 
     data['Y'] = data['cnt'].shift(-1)
     data = data.dropna()
@@ -32,9 +29,46 @@ def train(data: pd.DataFrame):
     lgbtrain = lgb.Dataset(data=X_train, label=Y_train, feature_name=list(X_train.columns), free_raw_data=False, categorical_feature='')
     lgbval = lgb.Dataset(data=X_val, label=Y_val, reference=lgbtrain, feature_name=list(X_val.columns), free_raw_data=False) if X_val is not None else None
 
+    params = {
+            'num_leaves': config.num_leaves,
+            'learning_rate': config.learning_rate,
+            'feature_fraction': config.feature_fraction,
+            'max_depth': config.max_depth,
+            'verbose': config.verbose,
+            'early_stopping_rounds': config.early_stopping_rounds,
+            'nthread': config.nthread
+        }
 
-if __name__ == "__main__":
-    data = pd.read_csv('C:\\Users\\iurin\\work\\itmo-hackathon\\coming_soon_ai_product_hack\\backend\\ML\\item_064.csv', parse_dates=['date'])
+    model = LGBMModel(params=params)
+    model.train(lgbtrain, lgbval)
+
+@hydra.main(version_base=None, config_path="../configs", config_name="training_default.yaml")
+def main(config: DictConfig):
+    """
+    Examples How to run:
+
+    python use_cases/hydra_with_pydantic.py
+    python use_cases/hydra_with_pydantic.py -cn training_default.yaml
+    python use_cases/hydra_with_pydantic.py batch_size=128
+    """
+    data = pd.read_csv(config.train_data_path)
     data = data[['cnt', 'date']] # date column is useful for feature generation
 
-    train(data)
+    if config.granularity == 'one_day':
+        day_params = one_day_params
+    
+    if config.granularity == 'seven_days':
+        pass
+
+    if config.granularity == 'thirty_days':
+        pass
+    
+    data_with_features = calculate_features_for_inference(data=data, params=day_params)
+    data_with_features = data_with_features.drop(columns=['date'])
+    data = data_with_features.copy()
+
+    run_training(data, config)
+
+
+if __name__ == "__main__":
+    main()
