c181b46a518c11cd5f3a19b10e0cfb64ea70f71c
Evgenii Iurin <iurin.evgenii@gmail.com>
2024-09-11 20:17:08 +0200

    Add inference script

--- backend/ml_pipeline/configs/feature_params.py
+++ backend/ml_pipeline/configs/feature_params.py
@@ -10,3 +10,31 @@ one_day_params = {
         'month': True, 
         'hour': False
 }
+
+seven_day_params = {
+        'target_column': 'cnt',               
+        'datetime_column': 'date',
+        'shift': 7,               
+        'lag_range': (1, 30),     
+        'lag_difference': (1, 30),
+        'rolling_mean_range': (5, 30),
+        'rolling_std_range': (5, 30), 
+        'day_of_week': True,          
+        'month': True,                
+        'hour': False                 
+}
+
+
+
+thirty_day_params = {
+        'target_column': 'cnt',
+        'datetime_column': 'date',
+        'shift': 30,              
+        'lag_range': (1, 90),     
+        'lag_difference': (1, 90),
+        'rolling_mean_range': (7, 90),
+        'rolling_std_range': (7, 90), 
+        'day_of_week': False,         
+        'month': True,                
+        'hour': False                 
+}
