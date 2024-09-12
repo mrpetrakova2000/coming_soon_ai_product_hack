fce44e569561fb874afdcdb7584d0a0c958a0a12
Ekaterina Bezpalaya <bezpalaya.katya@gmail.com>
2024-09-12 21:31:17 +0300

    improved_2

--- backend/merging_datasets/main.py
+++ backend/merging_datasets/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from preprocessing import prepare_shop_sales, prepare_shop_sales_dates, prepare_shop_sales_prices
-from utils import load_dataset, safe_merge
+from backend.merging_datasets.preprocessing import prepare_shop_sales, prepare_shop_sales_dates, prepare_shop_sales_prices
+from backend.merging_datasets.utils import load_dataset, safe_merge
 
 def merging(shop_sales_path, shop_sales_dates_path, shop_sales_prices_path):
     shop_sales = load_dataset(shop_sales_path)
@@ -20,11 +20,14 @@ def merging(shop_sales_path, shop_sales_dates_path, shop_sales_prices_path):
     ### Возвращаем датасет пользователю
         return None
     else:
-        cat_cols = ['item_id', 'store_id', 'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2', 'cashback_store_1', 'cashback_store_2', 'cashback_store_3']
+        cat_cols = ['item_id', 'store_id', 'cashback_store_1', 'cashback_store_2', 'cashback_store_3']
         num_cols = ['cnt', 'sell_price', 'wday', 'month', 'year']
+        event_cat_cols = ['event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
         for col in cat_cols:
             merged_df.loc[:,col] = merged_df[col].fillna('ffill')
             merged_df.loc[:,col] = merged_df[col].fillna('bfill')
         for col in num_cols:
             merged_df.loc[:,col] = merged_df[col].interpolate()
+        for col in event_cat_cols:
+            merged_df.loc[:,col] = merged_df[col].fillna('Unknown')
         return merged_df
