2aa3b0476f2b7e74f427fca25fd87b806c40239e
mrpetrakova2000 <mrpetrakova2000@gmail.com>
2024-09-13 13:48:40 +0300

    plots and dashboard for analytics

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,24 +12,6 @@ from backend.app.prediction import *
 from backend.app.cluster_prediction import *
 from backend.merging_datasets.main import merging
 
-
-# from backend.ML.dataset import LGBMDataset
-# from backend.ML.model import LGBMModel
-
-
-# file_path = 'merged_df.csv'
-# dataset = LGBMDataset(file_path)
-
-# model = LGBMModel()
-# model.train(dataset.lgbtrain, dataset.lgbval)
-# print("model trained")
-
-# model.load_model("new_model.txt")
-# print("model loaded")
-
-# model.save_model("new_model.txt")
-# print("model saved")
-
 app = FastAPI()
 
 origins = [
@@ -46,13 +28,6 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# Load the model
-# with open("app/churn_model.pkl", "rb") as f:
-#     model = pickle.load(f)
-
-
-# plots = [standart_plot(x1, y1, x2, y2, title, x_axis_title, y_axis_title, trace1_name, trace2_name)]
-
 @app.post("/getSkus/")
 async def getSku(files: List[UploadFile] = File(...), prediction_period: int = Form(...)):
     merged_df = fetch_merged_df(files)
@@ -64,11 +39,9 @@ async def getSku(files: List[UploadFile] = File(...), prediction_period: int = F
 
 @app.post("/getClusters/")
 async def getClusters():
-    with open('categories.csv', encoding="utf-8") as f:
+    with open('backend/app/categories.csv', encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=';')
         clusters = [item for sublist in reader for item in sublist]
-
-    print(clusters)
 
     return {"message": "Получение категорий продуктов"
      ,
@@ -77,32 +50,9 @@ async def getClusters():
 
 @app.post("/prediction/")
 async def prediction(files: List[UploadFile] = File(...), prediction_period: int = Form(...), choosed_sku: str = Form(...)):
-    # df = read_data()
-    # model.predict(df)
-    # data = prepare_data_fo_plot_from_original_dataset()
-    # predict = prepare_data_fo_plot_from_predict()
-    # metrics = extract_metrics(predict)
-
-    # for file in files:
-    #     print(file.filename)
-    
-    # print("prediction period")
-    # print(prediction_period)
-
-    # print("length of prediction")
-    # y = model.predict(dataset.X_val)
-    # f = open('file.txt', 'w')
-    # for y_i in y:
-    #     f.write(str(y_i) + ' ')
-    # f.close()
-    #print(model.predict(dataset.X_val))
-
-    # print("end predict")
 
     merged_df = fetch_merged_df(files)
     merged_df = merged_df.loc[merged_df['item_id'] == choosed_sku]
-    # print(merged_df)
-    # print(run_inference_on_sku(merged_df))
     
     return fetch_prediction(merged_df)
 
@@ -110,8 +60,8 @@ async def prediction(files: List[UploadFile] = File(...), prediction_period: int
 @app.post("/analytics/")
 async def analytics(files: List[UploadFile] = File(...), prediction_period: int = Form(...), choosed_sku: str = Form(...)):
     merged_df = fetch_merged_df(files)
-    merged_df = merged_df.loc[merged_df['item_id'] == choosed_sku]
-    return fetch_analytics(merged_df)
+    merged_df_sku = merged_df.loc[merged_df['item_id'] == choosed_sku]
+    return fetch_analytics(merged_df_sku, merged_df)
 
 @app.post("/clustering/")
 async def clustering(choosed_cluster: str = Form()):
@@ -123,7 +73,7 @@ def fetch_merged_df(files):
     shop_sales = fetch_file_by_fullmatch_filename_to_regex(files, r'shop_sales(?!.*(?:prices|dates)).*\.csv$')
     shop_sales_dates = fetch_file_by_fullmatch_filename_to_regex(files, r'shop_sales_dates\w*\.csv$')
     shop_sales_prices = fetch_file_by_fullmatch_filename_to_regex(files, r'shop_sales_prices\w*\.csv$')
-    print(shop_sales, shop_sales_dates, shop_sales_prices)
+
     merged_df = merging(shop_sales.file, shop_sales_dates.file, shop_sales_prices.file)
     return merged_df
 
